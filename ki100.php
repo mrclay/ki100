3cc62da13a5d8c865f478ebf233def346fbbd97d
Steve Clay <steve@mrclay.org>
2023-06-25 23:00:23 -0400

    2 spaces

--- ki100.php
+++ ki100.php
@@ -1,146 +1,145 @@
 <?php
 namespace ki100; // forgive me
 class Container {
-    private $_factories = [];
-    private $_values = [];
-    function setFactory($name, $factory) {
-        $this->_factories[$name] = $factory;
+  private $_factories = [];
+  private $_values = [];
+  function setFactory($name, $factory) {
+    $this->_factories[$name] = $factory;
+  }
+  function __get($name) {
+    if (!array_key_exists($name, $this->_values)) {
+      if (!array_key_exists($name, $this->_factories)) {
+        return null;
+      }
+      $this->_values[$name] = call_user_func($this->_factories[$name]);
     }
-    function __get($name) {
-        if (!array_key_exists($name, $this->_values)) {
-            if (!array_key_exists($name, $this->_factories)) {
-                return null;
-            }
-            $this->_values[$name] = call_user_func($this->_factories[$name]);
-        }
-        return $this->_values[$name];
-    }
+    return $this->_values[$name];
+  }
 }
 class Core extends Container {
-    private $listenerWrappers = [];
-    private $functionFiles = [];
-    /** @var Plugin[] */
-    private $plugins = [];
-    function addPlugins($dir) {
-        foreach (scandir($dir) as $entry) {
-            if ($entry[0] !== '.' && is_file("$dir/$entry/setup.php")) {
-                if (is_file("$dir/$entry/vendor/autoload.php")) {
-                    require "$dir/$entry/vendor/autoload.php";
-                }
-                $id = preg_replace('~^\\d+_~', '', $entry);
-                $plugin = new Plugin($id, "$dir/$entry");
-                $this->plugins[$plugin->id] = $plugin;
-                $factory = $this->requireFile("$dir/$entry/setup.php");
-                if (is_callable($factory)) {
-                    $factory($this, $plugin);
-                }
-                if (is_dir("$dir/$entry/functions")) {
-                    $this->findFunctionFiles("$dir/$entry/functions", "");
-                }
-            }
+  private $listenerWrappers = [];
+  private $functionFiles = [];
+  /** @var Plugin[] */
+  private $plugins = [];
+  function addPlugins($dir) {
+    foreach (scandir($dir) as $entry) {
+      if ($entry[0] !== '.' && is_file("$dir/$entry/setup.php")) {
+        if (is_file("$dir/$entry/vendor/autoload.php")) {
+          require "$dir/$entry/vendor/autoload.php";
         }
-    }
-    function call($name, array $args = [], $default = null) {
-        $args = $this->triggerEvent("call_args:$name", $args);
-        if (array_key_exists('__cancel', $args)) {
-            return $args['__cancel'];
+        $id = preg_replace('~^\\d+_~', '', $entry);
+        $plugin = new Plugin($id, "$dir/$entry");
+        $this->plugins[$plugin->id] = $plugin;
+        $factory = $this->requireFile("$dir/$entry/setup.php");
+        if (is_callable($factory)) {
+          $factory($this, $plugin);
         }
-        if (isset($this->functionFiles[$name])) {
-            if (substr($this->functionFiles[$name], -4) === '.php') {
-                ob_start();
-                $value = $this->requireFile($this->functionFiles[$name], array_merge($args, ['core' => $this]));
-                $output = ob_get_clean();
-                if ($output !== '') {
-                    $value = $output;
-                }
-            } else {
-                $value = file_get_contents($this->functionFiles[$name]);
-            }
-        } else {
-            $value = $default;
+        if (is_dir("$dir/$entry/functions")) {
+          $this->findFunctionFiles("$dir/$entry/functions", "");
         }
-        return $this->triggerEvent("call:$name", $value, $args);
+      }
     }
-    function addListener($eventName, callable $listener, $timing = 0) {
-        $this->listenerWrappers[] = [$eventName, $listener, (int)$timing];
+  }
+  function call($name, array $args = [], $default = null) {
+    $args = $this->triggerEvent("call_args:$name", $args);
+    if (array_key_exists('__cancel', $args)) {
+      return $args['__cancel'];
     }
-    function triggerEvent($eventName, $value = null, array $data = []) {
-        $event = new Event($value);
-        $event->name = $eventName;
-        $event->data = $data;
-        return $this->callListeners($event)->value;
-    }
-    function callListeners(Event $event) {
-        $event->core = $this;
-        foreach ($this->getListeners($event->name) as $listener) {
-            $listener($event);
-            if ($event->stopped) {
-                break;
-            }
+    if (isset($this->functionFiles[$name])) {
+      if (substr($this->functionFiles[$name], -4) === '.php') {
+        ob_start();
+        $value = $this->requireFile($this->functionFiles[$name], array_merge($args, ['core' => $this]));
+        $output = ob_get_clean();
+        if ($output !== '') {
+          $value = $output;
         }
-        return $event;
+      } else {
+        $value = file_get_contents($this->functionFiles[$name]);
+      }
+    } else {
+      $value = $default;
     }
-    function getListeners($eventName) {
-        $listenerSets = [];
-        foreach ($this->listenerWrappers as $wrapper) {
-            list ($registeredName, $listener, $timing) = $wrapper;
-            if ($registeredName === $eventName
-                    || ($registeredName[0] === '~' && preg_match($registeredName, $eventName))) {
-                $listenerSets[$timing][] = $listener;
-            }
-        }
-        ksort($listenerSets);
-        $allListeners = [];
-        foreach ($listenerSets as $listeners) {
-            $allListeners = array_merge($allListeners, $listeners);
-        }
-        return $allListeners;
+    return $this->triggerEvent("call:$name", $value, $args);
+  }
+  function addListener($eventName, callable $listener, $timing = 0) {
+    $this->listenerWrappers[] = [$eventName, $listener, (int) $timing];
+  }
+  function triggerEvent($eventName, $value = null, array $data = []) {
+    $event = new Event($value);
+    $event->name = $eventName;
+    $event->data = $data;
+    return $this->callListeners($event)->value;
+  }
+  function callListeners(Event $event) {
+    $event->core = $this;
+    foreach ($this->getListeners($event->name) as $listener) {
+      $listener($event);
+      if ($event->stopped) {
+        break;
+      }
     }
-    function getPlugin($id) {
-        return isset($this->plugins[$id]) ? $this->plugins[$id] : null;
+    return $event;
+  }
+  function getListeners($eventName) {
+    $listenerSets = [];
+    foreach ($this->listenerWrappers as $wrapper) {
+      list ($registeredName, $listener, $timing) = $wrapper;
+      if ($registeredName === $eventName || ($registeredName[0] === '~' && preg_match($registeredName, $eventName))) {
+        $listenerSets[$timing][] = $listener;
+      }
     }
-    function getPlugins() {
-        return $this->plugins;
+    ksort($listenerSets);
+    $allListeners = [];
+    foreach ($listenerSets as $listeners) {
+      $allListeners = array_merge($allListeners, $listeners);
     }
-    function requireFile($_file, array $scope = []) {
-        foreach ($scope as $key => $value) {
-            if (is_int($key)) {
-                unset($scope[$key]);
-                $scope["arg{$key}"] = $value;
-            }
-        }
-        unset($key, $value);
-        extract($scope);
-        return (require $_file);
+    return $allListeners;
+  }
+  function getPlugin($id) {
+    return isset($this->plugins[$id]) ? $this->plugins[$id] : null;
+  }
+  function getPlugins() {
+    return $this->plugins;
+  }
+  function requireFile($_file, array $scope = []) {
+    foreach ($scope as $key => $value) {
+      if (is_int($key)) {
+        unset($scope[$key]);
+        $scope["arg{$key}"] = $value;
+      }
     }
-    function h($text) {
-        return htmlspecialchars($text, ENT_QUOTES);
+    unset($key, $value);
+    extract($scope);
+    return (require $_file);
+  }
+  function h($text) {
+    return htmlspecialchars($text, ENT_QUOTES);
+  }
+  private function findFunctionFiles($dir, $prefix) {
+    foreach (scandir($dir) as $entry) {
+      if ($entry[0] === '.') {
+        continue;
+      }
+      if (is_dir("$dir/$entry")) {
+        $this->findFunctionFiles("$dir/$entry", "{$prefix}$entry/");
+      } else {
+        $this->functionFiles[$prefix . basename($entry, '.php')] = "$dir/$entry";
+      }
     }
-    private function findFunctionFiles($dir, $prefix) {
-        foreach (scandir($dir) as $entry) {
-            if ($entry[0] === '.') {
-                continue;
-            }
-            if (is_dir("$dir/$entry")) {
-                $this->findFunctionFiles("$dir/$entry", "{$prefix}$entry/");
-            } else {
-                $this->functionFiles[$prefix . basename($entry, '.php')] = "$dir/$entry";
-            }
-        }
-    }
+  }
 }
 class Plugin extends Container {
-    public function __construct(
-      readonly string $id,
-      readonly string $dir
-    ) {}
+  public function __construct(
+    readonly string $id,
+    readonly string $dir
+  ) {}
 }
 class Event {
-    public $name, $value, $originalValue, $data = [], $stopped = false;
-    /* @var Core */
-    public $core;
-    public function __construct($value = null) {
-        $this->value = $value;
-        $this->originalValue = $value;
-    }
+  public $name, $value, $originalValue, $data = [], $stopped = false;
+  /* @var Core */
+  public $core;
+  public function __construct($value = null) {
+    $this->value = $value;
+    $this->originalValue = $value;
+  }
 }
